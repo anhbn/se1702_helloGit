808c544febef666911754ccbf2e24498748bff86
ADMIN <ADMIN@DESKTOP-3S146JU>
2022-07-20 09:50:10 +0700

    Sửa lớp Main, thêm method displayGreeting()

--- Git01_Hello/src/Main.java
+++ Git01_Hello/src/Main.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ADMIN
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    public void displayGreeting() {
+        System.out.println("Hello SE1702. Today is Wednesday.");
+    
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Main().displayGreeting();
     }
     
 }
